d54f9aca4016c52d8da3fcf82963d813c66b34a4
barathrahav <barathrahav@gmail.com>
2024-07-06 17:54:26 +0530

    Assignment 1

--- ellty_task/src/App.js
+++ ellty_task/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css';
 
-function App() {
+const App = () => {
+  const [selectedPages, setSelectedPages] = useState({
+    allPages: false,
+    page1: false,
+    page2: false,
+    page3: false,
+    page4: false,
+  });
+
+  const handleCheckboxChange = (name) => {
+    setSelectedPages((prevSelectedPages) => ({
+      ...prevSelectedPages,
+      [name]: !prevSelectedPages[name],
+    }));
+  };
+
+  const handleDone = () => {
+    console.log('Selected pages:', selectedPages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="checkbox-container">
+      <label
+        className="checkbox-label"
+        onDoubleClick={() => handleCheckboxChange('allPages')}
+      >
+        All pages
+        <input
+          type="checkbox"
+          name="allPages"
+          checked={selectedPages.allPages}
+          readOnly
+        />
+        <span className="hover-checkmark"></span>
+      </label>
+      <div className="separator"></div>
+      <label
+        className="checkbox-label"
+        onDoubleClick={() => handleCheckboxChange('page1')}
+      >
+        Page 1
+        <input
+          type="checkbox"
+          name="page1"
+          checked={selectedPages.page1}
+          readOnly
+        />
+        <span className="hover-checkmark"></span>
+      </label>
+      <label
+        className="checkbox-label"
+        onDoubleClick={() => handleCheckboxChange('page2')}
+      >
+        Page 2
+        <input
+          type="checkbox"
+          name="page2"
+          checked={selectedPages.page2}
+          readOnly
+        />
+        <span className="hover-checkmark"></span>
+      </label>
+      <label
+        className="checkbox-label"
+        onDoubleClick={() => handleCheckboxChange('page3')}
+      >
+        Page 3
+        <input
+          type="checkbox"
+          name="page3"
+          checked={selectedPages.page3}
+          readOnly
+        />
+        <span className="hover-checkmark"></span>
+      </label>
+      <label
+        className="checkbox-label"
+        onDoubleClick={() => handleCheckboxChange('page4')}
+      >
+        Page 4
+        <input
+          type="checkbox"
+          name="page4"
+          checked={selectedPages.page4}
+          readOnly
+        />
+        <span className="hover-checkmark"></span>
+      </label>
+      <div className="separator"></div>
+      <button className="done-button" onClick={handleDone}>Done</button>
     </div>
   );
-}
+};
 
 export default App;
